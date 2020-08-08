e1f74e189180d1bffa87af5911d91828672df597
Amarnath Sakthivel <63423850+Amarnath-Sakthivel@users.noreply.github.com>
2020-08-08 18:43:11 +0530

    Add files via upload

---
+++ DecodeEncode.java
@@ -0,0 +1,36 @@
+package com.wipro.javapractice;
+
+public class DecodeEncode {
+	public static int[] encrypt(String str) {
+		int[] arr = new int[str.length()];
+		arr[0] = (int) str.charAt(0);
+		for (int i = 1; i < str.length(); i++) {
+			arr[i] = ((int) str.charAt(i) - (int) str.charAt(i - 1));
+		}
+		for (int i : arr) {
+			System.out.println(i);
+		}
+		return arr;
+
+	}
+
+	public static String decrypt(int[] arr) {
+
+		String decrypt="";
+		int sum=0;
+		for (int i = 0; i < arr.length; i++) {
+			sum+=arr[i];
+             decrypt+=(char)sum;
+		}
+		System.out.println(decrypt);
+
+		return null;
+
+	}
+
+	public static void main(String args[]) {
+		decrypt(new int[] { 72, 33, -73, 84, -12, -3, 13, -13, -68 });
+		encrypt("Hello");
+		encrypt("Sunshine");
+	}
+}
